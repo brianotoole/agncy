2123df0b2de57dcf05c7cc70216de8f0f61e8047
Brian O'Toole <brian@sparxoo.com>
2017-06-20 13:55:36 -0400

    close on esc

--- src/js/index.js
+++ src/js/index.js
@@ -9,6 +9,13 @@ $('#nav-toggle').on('click', function() {
   //$('.line').toggleClass('fillWidth');
 });
 
+$(document).keyup(function(e) {
+  var body = $('body');
+  if (body.is('.nav-open') && e.keyCode == '27') {
+    $('body').toggleClass('nav-open');
+  }
+});
+
 // get shape of svg for anim
 /*
 var shape = document.querySelector('#lines');
